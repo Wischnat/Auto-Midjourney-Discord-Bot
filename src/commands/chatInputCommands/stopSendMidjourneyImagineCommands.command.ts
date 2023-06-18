fd4f603bf044747f8783cc5817f9fdd98f07c461
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-06-18 21:03:31 +0200

    Add StopMidjourneyImagineCommands #3
    This command prevents sending /imagine commands.

---
+++ src/commands/chatInputCommands/stopSendMidjourneyImagineCommands.command.ts
@@ -0,0 +1,17 @@
+import { CommandInteraction } from "discord.js";
+import { MidjourneyImagineCommandSender } from "../../apis/midjourney/midjourneyImagineCommandSender";
+import { ChatInputCommand } from "../../types/chatInputCommand";
+
+const run = async (interaction: CommandInteraction) => {
+  const midjourneyImagineCommandSender =
+    await MidjourneyImagineCommandSender.getInstance();
+  midjourneyImagineCommandSender.enableCommandSending = false;
+
+  return await interaction.editReply("Sending commands has been stopped");
+};
+
+export const StopMidjourneyImagineCommands: ChatInputCommand = {
+  name: "stop-midjourney-imagine-commands",
+  description: 'stop "/imagine" midjourney commands',
+  run,
+};
