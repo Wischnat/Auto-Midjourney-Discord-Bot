0d881d07320f354adc6f33cf54aeb531027261ee
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-06-24 19:03:02 +0200

    remove ping command

--- src/commands/chatInputCommands/index.ts
+++ src/commands/chatInputCommands/index.ts
@@ -1,4 +1,3 @@
-export * from "./ping.command";
 export * from "./sendMidjourneyImagineCommands.command";
 export * from "./stopSendMidjourneyImagineCommands.command";
 export * from "./setLimit.command";
