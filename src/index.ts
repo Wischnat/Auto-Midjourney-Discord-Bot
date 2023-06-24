6b654a141eca514d283d37ccf76875e364147083
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-06-24 18:16:41 +0200

    add error handler #3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits } from "discord.js";
 import ready from "./events/ready.event";
 import interactionCreate from "./events/interactionCreate.event";
 import config from "./config/config";
+import errorHandler from "./handler/error.handler";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -9,5 +10,6 @@ const client = new Client({
 
 ready(client);
 interactionCreate(client);
+errorHandler(client);
 
 client.login(config.MY_BOT_DISCORD_TOKEN);
