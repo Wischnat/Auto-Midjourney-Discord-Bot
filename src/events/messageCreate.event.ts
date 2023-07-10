75ca5bdb4f3e832ac37e18b44ba36109aa3f2b8b
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-07-10 19:39:35 +0200

    Add message handler #5

--- src/events/messageCreate.event.ts
+++ src/events/messageCreate.event.ts
@@ -1,10 +1,11 @@
 import { Client, Message, MessageType } from "discord.js";
+import { handleMessage } from "../handler";
 
 // https://stackoverflow.com/questions/69539318/discord-js-bot-not-firing-events-when-a-message-is-sent-after-update
 export const messageCreateEvent = (client: Client): void => {
   client.on("messageCreate", async (message: Message) => {
     if (message.type === MessageType.Default) {
-      //handler
+      handleMessage(message);
     }
   });
 };
---
+++ src/handler/message.handler.ts
@@ -0,0 +1,8 @@
+import { Message } from "discord.js";
+
+export const handleMessage = (message: Message) => {
+
+  if (!message.author.bot) return;
+
+  // TODO: attachmentMessage
+};
